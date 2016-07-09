d2ae6740c69c12dee27d422065a984ab66e45242
Holstiche <matthijs.toa@gmail.com>
2016-07-09 22:30:25 +0200

    created basic file

---
+++ http_proxy.go
@@ -0,0 +1,13 @@
+//This is executable code so pakage is main
+package main
+
+//all needed packages should be imported here
+import (
+	//"example_import"
+)
+
+//other code van come here
+
+func main() {
+	//our main code shloud come here
+}
