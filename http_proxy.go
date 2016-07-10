f8f64bd14054368c8fe6b84d1ebf2f5f91796f6f
Holstiche <matthijs.toa@gmail.com>
2016-07-10 12:01:58 +0200

    added error handling, get request, the response to the client, url-argument validation and other methods still need to be implemented

--- http_proxy.go
+++ http_proxy.go
@@ -1,20 +1,37 @@
-//This is executable code so pakage is main
+//This is executable code so package is main
 package main
 
 //all needed packages are imported here
 import (
 	"fmt"
 	"net/http"
+	"log"
 )
 
 //Here our handler function is definded
 func handler(w http.ResponseWriter, r *http.Request) {
+	//The url of the server the client wants to acces via the proxy is passed as a url-argument
 	url := r.URL.Path[1:]
-	fmt.Fprintf(w, url)
+	//wordt gebruikt voor debuggen
+	fmt.Fprintf(w, url)	
+	//url validatie en flexibiliteit moet nog geimplementeerd worden
+
+	if r.Method == "GET" {
+		fmt.Fprintf(w, "r.Method is GET")
+		resp, err := http.Get(url)
+		//error handling
+		if err != nil {
+			log.Fatal(err)
+		}
+		//is dit wel echt nodig???
+		defer resp.Body.Close()
+	}
 }
 
-//our main code shloud come here
+
 func main() {
+	//The handler function is bound to the "/" url
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	//The server is created
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
